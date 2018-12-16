952a7b83d9d245da7f8aa367b8be8ef7aae1a3b1
Desmend Jetton <djetton@iit.edu>
2018-12-16 02:36:46 -0600

    corrected main.js directory and put in js folder.

---
+++ dist/js/main.js
@@ -0,0 +1,37 @@
+//Select DOM objects to make second page.
+
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuNav = document.querySelector(".menu-nav");
+const menuBranding = document.querySelector(".menu-branding");
+const navItems = document.querySelectorAll(".nav-item");
+
+//Start the Beginning State of the Menu
+let showMenu = false;
+
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  //check to see if the menu is clicked
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuBranding.classList.add("show");
+    //Foreach function since we have multiple elements named item.
+    navItems.forEach(item => item.classList.add("show"));
+
+    // Set Menu State
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBranding.classList.remove("show");
+    //Foreach function since we have multiple elements named item.
+    navItems.forEach(item => item.classList.remove("show"));
+
+    // Set Menu State
+    showMenu = false;
+  }
+}
